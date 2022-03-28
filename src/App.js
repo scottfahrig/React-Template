ed76ee4bf32dd3a3c96f51ad593dfe877c9b9cc0
Scott Fahrig <smfahrig@gmail.com>
2022-03-28 01:35:31 -0500

    Fixed tailwindcss misconfiguration.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Uncomment the <code>Dashboard</code> component in <code>App.js</code> to get started.</h1>
+      {/* <Dashboard /> */}
     </div>
   );
 }
